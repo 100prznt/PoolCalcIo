b1c4568587e70cb06e6cef169e6cb0eab20201a9
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2019-03-19 12:05:54 +0100

    JS test

--- scr/calc.php
+++ scr/calc.php
@@ -6,4 +6,15 @@
  * Time: 16:08
  */
 
+date_default_timezone_set("Europe/Berlin");
+$timestamp = time();
+
+
+$system = ['hostname' => 'PUMP001', 'state' => -1];
+$service = ['hostname' => 'ONDEV001', 'state' => 1, 'error' => 1003, 'error_message' => 'Network connection to subsystem not established.'];
+$data = [ 'version' => '1.0.17', 'timestamp' => date('c'), 'service' => $service, 'subsystem' => $system ];
+
+header('Content-type: application/json');
+echo json_encode( $data );
+
 die();
